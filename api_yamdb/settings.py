d8077beef2e2077696d3e8e4bb77e2302d1d0c6b
Zaluznyak <Zaluznyak@github.com>
2021-05-15 23:00:01 +0500

    modefied

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -131,4 +131,4 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-FROM_EMAIL = 'webmaster@yamdb.ru'
+FROM_EMAIL = 'webmasters@yamdb.ru'
